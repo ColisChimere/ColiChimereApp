c76a753daf502eb3a57de8dcfaeb16b39831fc10
BrivaelFer <brivaelsunny@orange.fr>
2024-03-24 15:59:56 +0100

    api commun end

--- public/js/registrationScript.js
+++ public/js/registrationScript.js
@@ -1,20 +1,34 @@
 
-const selectCP = $('#codePostaleForm');
-const selectCommune = $('#destinataire_form_adr_communSIREN');
-
-selectCP.on('change', function(){
+const selectCP = $('#codePostaleForm');//input cope postal
+const selectCommune = $('#destinataire_form_adr_communSIREN');//input commun
+//Change les options
+selectCP.on('change', async function(){
     if(this.value.length > 1){
         RemoveOption();
-        AddOptionCommun(RequestCommune(this.value));
+        AddOptionCommun(await RequestCommune(this.value));
     }
 });
+/**
+ * Retournz une liste de commune siren et nom
+ * @param {string} codeP code postale
+ * @returns 
+ */
+async function RequestCommune(codeP){
+    // const request = new XMLHttpRequest();
+    // request.open('GET', "https://geo.api.gouv.fr/communes?codePostal=" + codeP, true );
+    // let rep;
+    // request.onload = function() {
+    //     rep = JSON.parse(request.response);
+    // }
+    // request.send();
+    const response = await fetch(`https://geo.api.gouv.fr/communes?codePostal=${codeP}`);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const rep = await response.json(); // Obtient le JSON de la réponse
 
-function RequestCommune(codeP){
-    const request = new XMLHttpRequest();
-    request.open('GET', "https://geo.api.gouv.fr/communes?codePostal=" + codeP, false );
-    let rep = JSON.parse(request.response);
     let res = [];
-    for(commun in rep){
+    for(commun of rep){
         res.push({
             "nom": commun.nom,
             "siren": commun.siren
@@ -22,15 +36,19 @@ function RequestCommune(codeP){
     }
     return res;
 }
-
+/**
+ * Ajout les option.
+ * @param {array} array list commune
+ */
 function AddOptionCommun(array){
-    for(commun in array){
+    for(commun of array){
         selectCommune.append($('<option>', {
             value: commun.siren,
             text: commun.nom
         }));
     }
 }
+/**Efface les option */
 function RemoveOption(){
-    selectCommune.innerHTMl = '';
+    selectCommune.empty();
 }
