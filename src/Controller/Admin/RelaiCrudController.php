80ae1b85a96b10c225470dcf39941c6de9b22b8c
GaetanEvain22 <147801276+GaetanEvain22@users.noreply.github.com>
2024-04-29 23:49:29 +0200

    wip (easyadmin) : creation des relais dans le  relaicrudcontroller

--- src/Controller/Admin/RelaiCrudController.php
+++ src/Controller/Admin/RelaiCrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Admin;
 
 use App\Entity\Relai;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -23,8 +24,8 @@ class RelaiCrudController extends AbstractCrudController
     {
         return [
             IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('adresse'),
+            TextField::new('nomRelai'),
         ];
     }
     
